64e16f795e865a4ed777b89730c1e418ff705f38
StefanosTzaf <stzaferis04@gmail.com>
2024-11-01 19:30:07 +0200

    tryingFirstPipe

--- second_assignment/root.c
+++ second_assignment/root.c
@@ -2,6 +2,7 @@
 #include <getopt.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 int main(int argc, char* argv[]) {
     if(argc < 13){
@@ -55,8 +56,34 @@ int main(int argc, char* argv[]) {
         fprintf(stderr, "Error opening file %s\n", inputFile);
         return 1;
     }
+    int lines = 1;
+    int bytesRead;
+    char buffer[4096];
+    //μετρημα γραμμών
+    while ((bytesRead = read(fdInput, buffer, sizeof(buffer))) > 0) {
+        for (int i = 0; i < bytesRead; i++) {
+            if (buffer[i] == '\n') {
+                lines++;
+            }
+        }
+    }
 
-    pid_t spliterPids[numOfSplitter];
+    //δείκτης ανάγνωσης στην αρχή του αρχείου
+    lseek(fdInput, 0, SEEK_SET);
+
+    //φτίαχνουμε pipes οσα και οι splitters
+    int pipes[numOfSplitter][2];
+
+    for (int i = 0; i < numOfSplitter; i++) {
+        if (pipe(pipes[i]) == -1) {
+            fprintf(stderr,"Error creating pipe %d\n", i);
+            exit(1);
+        }
+    }
+
+    //δημιουργούμε l splitters με lines/l γραμμές το καθένα
+    int linesForSplitter = lines / numOfSplitter;
+    pid_t splitterPids[numOfSplitter];
 
     for (int i = 0; i < numOfSplitter; i++) {
         pid_t pid = fork();
@@ -65,24 +92,56 @@ int main(int argc, char* argv[]) {
             fprintf(stderr, "Error forking splitter %d\n", i);
             exit(EXIT_FAILURE);
         }
+        //splitter
         else if (pid == 0){
+            //κλείνουμε το γράψιμο στουςς splitter
+            close(pipes[i][1]);
+            dup2(pipes[i][0], STDIN_FILENO);
+            close(pipes[i][0]);
 
-            // Εδώ μπορείς να εκτελέσεις το πρόγραμμα του splitter
-            // execvp("splitter_program", args);
-            // Για παράδειγμα:
+            execlp("./splitter", "./splitter", NULL);
+            fprintf(stderr, "Error executing splitter %d\n", i);
             exit(EXIT_SUCCESS); // Τερματισμός του splitter
         } 
         else {
+            close(pipes[i][0]);
+            int startLine = i * linesForSplitter;
+            //αν είναι η τελευταία γραμμή τότε το τελευταίο splitter θα πάρει τις υπόλοιπες γραμμές
+            //(μπορεί να μην διαιρείται ακριβώς με τον αριθμό των splitters και να υπάρχουν κάποιες παραπάνω γραμμές)
+            int endLine = (i == numOfSplitter - 1) ? lines : (i + 1) * linesForSplitter;
+
+            //Γράφουμε στο pipe του splitter τις γραμμές που του αναλογούν
+            int currentLine = 0;
+            char buffer[4096];
+            while (read(fdInput, buffer, sizeof(buffer)) > 0) {
+                for (int j = 0; j < sizeof(buffer); j++) {
+                    if (buffer[j] == '\n') {
+                        currentLine++;
+                    }
+                    if (currentLine >= startLine && currentLine < endLine) {
+                        write(pipes[i][1], buffer + j, 1);
+                    }
+                    if (currentLine >= endLine) {
+                        break;
+                    }
+                }
+                if (currentLine >= endLine) {
+                    break;
+                }
+            }
+
+
+            close(pipes[i][1]);
             // Στον γονέα, αποθηκεύουμε το PID του splitter
-            spliterPids[i] = pid;
+            splitterPids[i] = pid;
         }
     }
 
     // Περιμένουμε όλους τους splitters να ολοκληρωθούν
     for (int i = 0; i < numOfSplitter; i++) {
-        waitpid(spliterPids[i], NULL, 0);
+        waitpid(splitterPids[i], NULL, 0);
     }
-    
+
 
     //στην αποδεύσμευση της λίστας θα καλείται η free για κάθε κόμβο
     //αρα θα απελευθερώνεται και ο χώρος που έχει δεσμευτεί για το string στην Main
---
+++ second_assignment/splitter.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main(int argc, char* argv[]){
+    //χωρίς ορίσματα(το αρχείο είναι ανοιχτό από τη κλήση της exec δεν το περνάμε σαν όρισμα)
+    if(argc != 1){
+        fprintf(stderr, "Usage: ./splitter <startLine> <endLine> <file descriptor>\n");
+        return 1;
+    }
+
+    return 0;
+}
