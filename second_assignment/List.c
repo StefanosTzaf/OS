70062f5c304cc3c353385c990d967e42704e81e7
StefanosTzaf <stzaferis04@gmail.com>
2024-11-01 02:13:04 +0200

    first

---
+++ second_assignment/List.c
@@ -0,0 +1,176 @@
+#include "List.h"
+
+//διπλα συνδεδεμενη λιστα
+//Η λιστα θα είναι ένας pointer σε αυτό το struct
+struct list {
+	ListNode head;
+	ListNode last;
+	int size;
+	DestroyFunc destroyValue;
+	CompareFunc compare;
+};
+
+struct list_node {
+	ListNode next;
+	ListNode prev;
+	Pointer value;
+};
+
+
+List listCreate(DestroyFunc destroyValue, CompareFunc compare) {
+	List list = malloc(sizeof(*list));
+	list->size = 0;
+	list->last = NULL;
+	list->head = NULL;
+	list->destroyValue = destroyValue;
+	list->compare = compare;
+	return list;
+}
+
+void listInsert(List list, Pointer value) {
+	ListNode new = malloc(sizeof(*new));
+	new->value = value;
+	new->next = NULL;	//προσθέτουμε τον κόμβο στο τέλος της λίστας
+	new->prev = list->last;	
+
+	//Εαν υπάρχει κόμβος μέσα στην λίστα
+	if (list->last != NULL){
+		list->last->next = new;
+	}
+	///αν η λιστα είναι κενή
+	else{
+		list->head = new;
+	}
+	list->last = new;
+	list->size++;
+}
+
+
+void listDeleteNode(List list, Pointer value){
+	ListNode node = listFind(list, value);
+	
+    // Έλεγχος αν η λίστα είναι άδεια
+    if (node == NULL) {
+	    return;
+    }
+
+	ListNode previousNode = node->prev;
+	ListNode nextNode = node->next;
+
+	//Ο μόνος κόμβος της λίστας είναι αυτός που διαγράφεται
+	if (previousNode == NULL && nextNode == NULL) {
+		list->head = NULL;
+		list->last = NULL;
+	}
+
+	//ο κόμβος ήταν στην αρχή αλλά έχει επόμενο
+	else if (previousNode == NULL) {
+		list->head = nextNode;
+		nextNode->prev = NULL;
+	}
+
+	//ο κόμβος ήταν στο τέλος αλλά έχει προηγούμενο
+	else if (nextNode == NULL) {
+		list->last = previousNode;
+		previousNode->next = NULL;
+	}
+
+	else {
+		previousNode->next = nextNode;
+		nextNode->prev = previousNode;
+	}
+
+	// Απελευθέρωση μνήμης του κόμβου που διαγράφεται
+	if(list->destroyValue != NULL){
+		list->destroyValue(node->value);
+	}
+
+	if(node != NULL){
+		free(node);
+		node = NULL;
+	}
+
+	list->size--;
+}
+
+
+void listRemoveLast(List list){
+	ListNode last = list->last;
+	if(last == NULL){
+		return;
+	}
+	ListNode previous = last->prev;
+	//Αν η λίστα έχει μόνο έναν κόμβο
+	if(previous == NULL){
+		list->head = NULL;
+	}
+	else{
+		previous->next = NULL;
+	}
+	list->last = previous;
+	list->size--;
+	if(list->destroyValue != NULL){
+		list->destroyValue(last->value);
+	}
+	free(last);
+}
+
+
+void listDestroy(List list) {
+	ListNode node = list->head;
+	while (node != NULL) {
+		ListNode next = node->next;
+		//κανουμε destroy τον κάθε κόμβπ βάση της συνάρτησης destroyValue
+		//(o Pointer μπορει να δειχνει σε διαφορετικο τύπο δεδομένων αρα κάθε φορά πρέπει να 
+		//χειριζίμαστε αλλιώς το free για να μην έχουμε leaks)
+		list->destroyValue(node->value);
+		node = next;
+	}
+
+	//Απελευθέρωση χώρου που έχει δεσμευτεί για τους κόμβους
+	node = list->head;
+	while (node != NULL) {
+		ListNode next = node->next;
+		free(node);
+		node = next;
+	}
+
+	//free τον χώρο που έχει δεσμευτεί για το struct
+	free(list);
+}
+
+
+//Αναζήτηση στην λίστα βάση της compare
+ListNode listFind(List list, Pointer value) {
+	// διάσχιση όλης της λίστας, καλούμε την compare μέχρι να επιστρέψει 0
+	for (ListNode node = list->head; node != NULL; node = node->next){
+		if (list->compare(value, listNodeValue(node)) == 0)
+			return node;
+	}
+	return NULL;
+}
+
+//για να μην υπάρχει πρόσβαση εκτος του αρχείου σε στοιχεία του struct list δημιουργούμε getters που θα χρειαστούν
+ListNode listGetFirst(List list){
+	return list->head;
+}
+
+ListNode listGetLast(List list){
+	return list->last;
+}
+
+ListNode listGetNext(ListNode node){
+	return node->next;
+}
+
+Pointer listNodeValue(ListNode node){
+	return node->value;	
+}
+
+int listSize(List list){
+	return list->size;
+}
+
+void listSetDestroyValue(List list, DestroyFunc destroyValue){
+	list->destroyValue = destroyValue;
+}
---
+++ second_assignment/Map.c
@@ -0,0 +1,114 @@
+#include "Map.h"
+
+//Κόμβος του hash table
+struct map_node{
+	char* keyId; 
+	Pointer value;    
+};
+
+struct map {
+	List* arrayOfBuckets;	    // Ο πίνακας των buckets του hash table(seperate chainening)
+	int capacity;				// Πόσο χώρο έχουμε δεσμεύσει εώς τώρα.
+	//συναρτήσεις που θα χρησιμοποιηθούν από την λίστα στα buckets
+	DestroyFunc destroyMapNodes;
+	CompareFunc compareMapNodes;
+};
+
+
+//η compare και η destroy για τις Lists απο MapNode
+Map mapCreate(CompareFunc compare, DestroyFunc destroy, int sizeByFile) {
+
+	Map map = malloc(sizeof(*map));
+	map->capacity = sizeByFile * 3 + 7;
+	map->arrayOfBuckets = malloc(sizeof(List) * map->capacity);
+	map->destroyMapNodes = destroy;
+	map->compareMapNodes = compare;
+
+	for (int i = 0; i < map->capacity; i++){
+		map->arrayOfBuckets[i] = NULL;
+	}
+	return map;
+}
+
+
+void mapInsert(Map map, char* key, Pointer value) {
+	int pos = hashFunction(key) % map->capacity; // mod για να μην ξεπεράσουμε το μέγεθος του πίνακα
+	
+	//αν είναι ο πρώτος κόμβος που hasharei σε αυτήν την θέση του πίνακα
+	if(map->arrayOfBuckets[pos] == NULL){
+		map->arrayOfBuckets[pos] = listCreate(map->destroyMapNodes, map->compareMapNodes);
+	}
+
+	MapNode node = malloc(sizeof(*node));
+	node->value = value;
+	node->keyId = key;
+
+	//Η Λιστα έχει MapNode
+	listInsert((map->arrayOfBuckets[pos]), node);
+}
+
+
+void mapRemove(Map map, char* key) {
+	MapNode node = mapFindNode(map, key);
+	if (node == NULL){
+		return;
+	}
+	else{
+		listDeleteNode(map->arrayOfBuckets[hashFunction(key) % map->capacity], node);
+	}
+}
+
+Pointer mapFind(Map map, char* key) {
+	MapNode node = mapFindNode(map, key);
+	if(node == NULL){
+		return NULL;
+	}
+	else{
+		return node->value;
+	}
+}
+
+//επιστρέφει τον κόμβο που αντιστοιχεί στο key , καλείται από την mapFind
+MapNode mapFindNode(Map map, char* key) {
+	int pos = hashFunction(key) % map->capacity;
+	
+	if( map->arrayOfBuckets[pos] == NULL){
+		return NULL;
+	}
+	//Η δουλεία της find θα γίνει ουσιαστικά από την compare που παραθέτουμε κάθε φορά στην λίστα
+	ListNode node = listFind(map->arrayOfBuckets[pos], key);
+	if(node == NULL){
+		return NULL;
+	}
+	else{
+		return listNodeValue(node);
+	}
+}
+
+
+void mapDestroy(Map map) {
+	for (int i = 0; i < map->capacity; i++) {
+		if(map->arrayOfBuckets[i] != NULL){
+			listDestroy(map->arrayOfBuckets[i]);
+		}
+	}
+	free(map->arrayOfBuckets);
+	free(map);
+}
+
+
+Pointer mapNodeKey(Map map, MapNode node) {
+	return node->keyId;
+}
+
+Pointer mapNodeValue(MapNode node) {
+	return node->value;
+}
+
+// djb2 hash function για strings (όπως αναφέρθηκε στο piazza επιτρέπεται να χρησιμοποιηθεί έτοιμη συνάρτηση)
+unsigned int hashFunction(char* value) {
+    unsigned int  hash = 5381;
+    for (char* s = value; *s != '\0'; s++)
+		hash = (hash << 5) + hash + *s;
+    return hash;
+}
