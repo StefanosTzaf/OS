76c73ec1917876ab2f1c1a785982ed1cec8ec5fe
StefanosTzaf <stzaferis04@gmail.com>
2024-11-14 02:19:17 +0200

    spl1

--- second_assignment/src/splitter.c
+++ second_assignment/src/splitter.c
@@ -12,18 +12,20 @@ int main(int argc, char* argv[]){
         fprintf(stderr, "Usage: ./splitter <inputFile> <startLine> <endLine> <numberOfBytes>\n");
         exit(1);
     }
+
     int fd = open(argv[1], O_RDONLY);
     if(fd == -1){
         perror("Error opening file");
         exit(1);
     }
+    
     // read the file from where this specific splitter starts
     int startLine = atoi(argv[2]);
     int endLine = atoi(argv[3]);
     int firstByteToRead = atoi(argv[4]);
 
+    // set the read pointer to the first byte of the splitter
     lseek(fd, firstByteToRead, SEEK_SET);
-    printf("Splitter id : %d\n", getpid());
 
     int linesToRead = endLine - startLine + 1;
 
@@ -32,91 +34,87 @@ int main(int argc, char* argv[]){
     bool allLinesRead = false;
     
     int bytesRead = read(fd, buffer, sizeof(buffer));
-
     while(bytesRead > 0){
 
         int size = 0;
-        // if a word ends with punctuation
-        int endsWithPunct = 0;
+        //the initial capacity for the word
         int capacity = 10;
-        // if a word is of the form λ3ξη$ then it is not valid
+        // if a word is of the form w#ord then it is not valid
         int valid = 1;
         int ch;
         char *word = malloc(capacity * sizeof(char));
 
-        for (int i = 0; i < bytesRead; i++) {
+        //----------------------------------------------- words' seperation and checks -----------------------------------------------
+        int i = 0; 
+        while(i < bytesRead){
             char ch = buffer[i];
             if (ch == '\n') {
                 currentLine++;
             }
             // if we reached the next line, after the last one, the splitter's job is done
-            if(currentLine == atoi(argv[3]) + 1){
+            if(currentLine == endLine + 1){
                 allLinesRead = true;
+                // Handle the last word
+                if(size > 0 && valid){
+                    word[size] = '\0';
+                    printf("Read valid word: %s\n", word);
+                }
                 break;
             }
-
+            
+            // if the character is a letter, add it to the word
             if (isalpha(ch)) {
-                // dynamic increase of word (we have no limit on the word size)
+            // dynamic increase of word (we have no limit on the word size)
                 if (size >= capacity - 1) {
                     capacity *= 2;
                     word = realloc(word, capacity * sizeof(char));
                 }
                 word[size++] = ch;
-                endsWithPunct = 0;
-                valid = 1;
-                continue;
             } 
-            // else if ((ch == '!' || ch == '?' || ch == '.') && (size > 0) && (valid)) {
-            //     endsWithPunct = 1;
-            //     word[size] = '\0';
-            //     printf("Read valid word: %s\n", word);
-            //     size = 0;
-            // } 
-            else if (isspace(ch) ) {
+            else if (ispunct(ch) && (size > 0) && (valid)) {
+                word[size] = '\0';
+                printf("Read valid word: %s\n", word);
+                size = 0;
+            } 
+            else if (ch == ' ' ||ch == '\n') {
                 if (size > 0 && valid) {
-                    if (size > 0) {
-                        word[size] = '\0';
-                        printf("Read valid word: %s\n", word);
-                    }
+                    word[size] = '\0';
+                    printf("Read valid word: %s\n", word);
                 }
                 // for the next word
                 size = 0;
                 valid = 1;
-                endsWithPunct = 0;
             } 
-            // else {
-            //     // in case we have an invalid character
-            //     valid = 0;
-            //     // skip characters until we find a space
-            //     while ((ch != EOF) && (ch != ' ' ) && (ch != '\n')) {
-            //         printf("Skipping invalid character: %c\n", ch);
-            //         ch = getchar();
-            //     }
-            //     size = 0;
+            else {
+                // in case we have an invalid character
+                valid = 0;
+                // skip characters until we find a space
+                while ((ch != ' ' ) && (ch != '\n')) {
+                    i++;
+                    ch = buffer[i];
+                }
+                
 
-            // }
-            
+            }
+            if(valid){
+                //if the character was not invalid, we move to the next character
+                i++;
+            }
         }
-        // Handle the last word if input didn't end with a space
-        // if (size > 0 && valid) {
-        //     if (endsWithPunct || size > 0) {
-        //         word[size] = '\0';
-        //         printf("Read valid word: %s\n", word);
-        //     }
-            
-
+        //--------------------------------------------------------------------------------------------------
+        if(size > 0 && valid){
+            word[size] = '\0';
+            printf("Read valid word: %s\n", word);
+        }
         free(word);
-
-
         if(allLinesRead){
             break;
         }
         // if we have read the entire buffer and have not read
         // the lines corresponding to the splitter, then read again
 
-        if(bytesRead == 0){
-            bytesRead = read(fd, buffer, sizeof(buffer));
-        }
+        bytesRead = read(fd, buffer, sizeof(buffer));
+    
 
     }
 
